3f1dedef317ad4b6bb8289c40b7794df1a32ef9d
abled007 <audreybledsoe3@gmail.com>
2022-02-22 18:38:29 -0800

    random aspect

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-$.ajax({
-    url: "https://anime-facts-rest-api.herokuapp.com/api/v1",
-}).then(
-    (data) => {
-        $('#name').html(data);
-        console.log(data)
-    },
-    (error) => {
-        console.log("bad request: ", error);
-    }
-);
+$('form').on('submit', (event) => {
+    event.preventDefault();
+    $.ajax({
+        url: "https://anime-facts-rest-api.herokuapp.com/api/v1",
+    }).then(
+        (data) => {
+            $('#name').html(data)
+            console.log(data);
+        },
+        (error) => {
+            console.log("bad request: ", error);
+        }
+    );
+})
+
+// random fact 
+
+async function getQuote(e) {
+    e.preventDefault();
+
+    const quote = fetch('https://animechan.vercel.app/api/random')
+            .then(response => response.json())
+            .then(quote => console.log(quote))
+            
+    $('#name').html(quote);
+}
+$("form").on("submit", getQuote);
+
 
 
 
