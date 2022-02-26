458c682d74362850fd03e86f56ff968afed63a92
abled007 <audreybledsoe3@gmail.com>
2022-02-26 11:27:05 -0800

    buttons work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('form').on('submit', (event) => {
+$('#myForm').on('submit', (event) => {
     event.preventDefault();
     $.ajax({
         url: "https://anime-facts-rest-api.herokuapp.com/api/v1",
@@ -16,32 +16,19 @@ $('form').on('submit', (event) => {
     );
 })
 
-// random fact 
-    
-    // $('form').on('submit', (event) => {
-    //     event.preventDefault();
-    //     fetch('https://animechan.vercel.app/api/random')
-    //         .then(response => response.json())
-    //         .then(quote => {
-    //             $('body').find('#quote').html(quote);
-    //             console.log(quote)
-    //         })
-    // })
+async function getData(e) {
+    e.preventDefault();
 
+    const url = 'https://animechan.vercel.app/api/random'
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    $("#anime").html(data.anime);
+    $("#character").html(data.character);
+    $("#quote").html(data.quote);
+    console.log(data);
 
+}
 
-    async function getData(e) {
-        e.preventDefault();
-    
-        const url = 'https://animechan.vercel.app/api/random'
-        const response = await fetch(url);
-        const data = await response.json();
-    
-        $("#anime").html(data.anime);
-        $("#character").html(data.character);
-        $("#quote").html(data.quote);
-        console.log(data)
-    }
-    
-    $("form").on("submit", getData);
+$("#form").on("submit", getData);
 
